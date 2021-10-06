36fbfcbfe6c12160edac7f89da3e5810c995be98
nicguzzo <nicguzzo@gmail.com>
2021-10-06 18:33:24 -0300

    v1.0

---
+++ src/main/java/net/fabricmc/deepslateinstamine/Mod.java
@@ -0,0 +1,15 @@
+package net.fabricmc.deepslateinstamine;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Mod implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("deepslateinstamine");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
