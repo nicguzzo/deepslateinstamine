cfffcc86defd5f7dedd97d62f4e3820f27822e21
nicguzzo <nicguzzo@gmail.com>
2023-10-05 01:13:06 -0300

    updated upload

--- upload.sh
+++ upload.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+#. ../wands_env_token
 pushd "versions"
-	mcvers=`ls |grep -P "mc1\..+"|tr "\n" " "|sed 's/mc//g'`
+#	mcvers=`ls |grep -P "mc1\..+"|tr "\n" " "|sed 's/mc//g'`
+
+	if [ "$1" == "" ]; then
+    	mcvers=`ls |grep -P "mc1\..+"|tr "\n" " "|sed 's/mc//g'`
+  	else
+	    mcvers=( $1 )
+  	fi
 	for v in ${mcvers[@]}; do
 		pushd mc$v
 			./gradlew --no-daemon publishUnified
